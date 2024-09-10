fad821a65f0b4aa6d10f15bfb4dff64b5ffe2d78
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-09-10 16:13:07 -0600

    Update core.py

--- core.py
+++ core.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+Conversion = ("Milímetros", "Metros", "Pies", "Pulgadas")
 Data = pd.read_excel("Material_data.xlsx")
 
 Material = Data["Material"].unique()
 
 sb_material = st.selectbox("Material:", Material)
+sb_conversion = st.selectbox("Unidades:" Conversion)
+
 
 df_front = Data[Data["Material"]==sb_material]
 
@@ -13,6 +16,8 @@ Condicion = df_front["Condicion"].unique()
 sb_condicion = st.selectbox("Condicion:", Condicion)
 
 df_front = df_front[df_front["Condicion"]==sb_condicion]
+
+
 
 st.write(df_front)
 
