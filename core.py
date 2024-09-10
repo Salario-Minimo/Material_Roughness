1fb214ddca1937d9e0dcba0e5dba2673c99c208e
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-09-10 02:27:29 -0600

    Update core.py

--- core.py
+++ core.py
@@ -3,4 +3,9 @@ import pandas as pd
 
 Data = pd.read_excel("Material_data.xlsx")
 
+Material = Data["Material"].unique()
+Condicion = Data["Condicion"].unique()
+
+st.write(Material, Condicion)
+
 st.write(Data)
