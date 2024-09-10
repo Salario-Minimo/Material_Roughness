c0675f08a3fe0243e59feac32bccc5694289e8c3
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-09-10 15:43:46 -0600

    Update core.py

--- core.py
+++ core.py
@@ -6,6 +6,9 @@ Data = pd.read_excel("Material_data.xlsx")
 Material = Data["Material"].unique()
 Condicion = Data["Condicion"].unique()
 
+sb_material = st.selectbox("Material:", Material)
+sb_condicion = st.selectbox("Condicion:", Condicion)
+
 st.write(Material, Condicion)
 
 st.write(Data)
