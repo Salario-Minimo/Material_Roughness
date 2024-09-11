e0a02e034000dd55a4a9312172fcb2b58c0b46a1
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-09-10 21:47:24 -0600

    Update core.py

--- core.py
+++ core.py
@@ -13,8 +13,6 @@ Conversion = {"Milímetros":1, "Metros":1000, "Pies":304.8, "Pulgadas":25.4}
 Data = pd.read_excel("Material_data.xlsx")
 
 sb_conversion = st.selectbox("Unidades:", Conversion)
-# Aquí ocurre la conversión
-Data["Minimo"] = Data["Minimo"]/Conversion[sb_conversion]
 Data["Minimo"] = Data.apply(scientific_notation, axis=1)
 
 Material = Data["Material"].unique()
