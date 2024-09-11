7c5c48ff7fee53dc862a693f7d12e593e8d41a8e
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-09-10 22:30:15 -0600

    Update core.py

--- core.py
+++ core.py
@@ -6,7 +6,7 @@ def scientific_notation(row):
   it = ("Minimo", "Maximo", "Recomendado")
   for x in it:
     row[x] = row[x]/Conversion[sb_conversion]
-    st.write(log10(abs(row[x])))
+    st.write(abs(log10(row[x])))
     row[x] = str(row[x]) + " " + Sufijos[sb_conversion]
   return row
 
