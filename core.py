cf9ba67f6a500270c0374487c090e91a12563c12
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-09-10 21:52:41 -0600

    Update core.py

--- core.py
+++ core.py
@@ -6,7 +6,7 @@ def scientific_notation(row):
   for x in it:
     row[x] = row[x]/Conversion[sb_conversion]
     row[x] = "{:.2e}".format(row[x])
-    row[x] = str(row[x]) + Sufijos[sb_conversion]
+    row[x] = str(row[x]) + " " + Sufijos[sb_conversion]
   st.write(row)
   return row
 
